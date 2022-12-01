d6b3d42362070e840764890c9c0f6286ceb20f96
Nick Cao <nickcao@nichi.co>
2022-12-01 23:54:46 +0800

    load package list from assets

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
 #[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
 pub fn main() {
-    let packages: Vec<&'static str> = vec![
-        "io.nekohasekai.sagernet",
-        "org.telegram.messenger.web",
-        "co.nichi.panic",
-    ];
+    let activity = ndk_glue::native_activity();
+    let mut packages = activity
+        .asset_manager()
+        .open(&std::ffi::CString::new("packages").unwrap())
+        .unwrap();
+    let packages = std::str::from_utf8(packages.get_buffer().unwrap()).unwrap();
     #[allow(unused_must_use)]
-    for package in packages {
+    for package in packages.lines() {
         std::process::Command::new("su")
             .args(["--command", "pm", "uninstall", package])
             .status();
     }
-    ndk_glue::native_activity().finish();
+    activity.finish();
 }
