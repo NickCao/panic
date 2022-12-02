bc2847f51169cdf059c556ef1d632bd6ef326ff6
Nick Cao <nickcao@nichi.co>
2022-12-02 11:54:52 +0800

    get package name with jni

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,38 @@
+use jni::errors::Result;
+use jni::objects::{JObject, JString, JValue};
+use jni::JavaVM;
+use ndk::native_activity::NativeActivity;
+use std::ffi::CString;
+use std::process::Command;
+
+pub fn package(activity: &NativeActivity) -> Result<String> {
+    let vm = unsafe { JavaVM::from_raw(activity.vm()) }?;
+    let ctx = unsafe { JObject::from_raw(activity.activity()) };
+    let env = vm.attach_current_thread()?;
+    let obj = env.call_method(ctx, "getPackageName", "()Ljava/lang/String;", &[])?;
+    if let JValue::Object(obj) = obj {
+        return Ok(env
+            .get_string(JString::from(obj))?
+            .to_str()
+            .unwrap()
+            .to_string());
+    } else {
+        unreachable!();
+    }
+}
+
 #[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
 pub fn main() {
     let activity = ndk_glue::native_activity();
+    let pkg = package(activity).unwrap();
     let mut packages = activity
         .asset_manager()
-        .open(&std::ffi::CString::new("packages").unwrap())
+        .open(&CString::new("packages").unwrap())
         .unwrap();
     let packages = std::str::from_utf8(packages.get_buffer().unwrap()).unwrap();
     #[allow(unused_must_use)]
-    for package in packages.lines() {
-        std::process::Command::new("su")
+    for package in packages.lines().chain([pkg.as_str()]) {
+        Command::new("su")
             .args(["--command", "pm", "uninstall", package])
             .status();
     }
