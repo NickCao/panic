1a617a35f202accf981b78ee4a299a7eef829afa
Nick Cao <nickcao@nichi.co>
2022-12-04 13:43:25 +0800

    also disable adb

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@ pub fn package(activity: &NativeActivity) -> Result<String> {
     }
 }
 
+#[allow(unused_must_use)]
 #[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
 pub fn main() {
     let activity = ndk_glue::native_activity();
@@ -30,11 +31,13 @@ pub fn main() {
         .open(&CString::new("packages").unwrap())
         .unwrap();
     let packages = std::str::from_utf8(packages.get_buffer().unwrap()).unwrap();
-    #[allow(unused_must_use)]
     for package in packages.lines().chain([pkg.as_str()]) {
         Command::new("su")
             .args(["--command", "pm", "uninstall", package])
             .status();
     }
+    Command::new("su")
+        .args(["--command", "settings", "put", "global", "adb_enabled", "0"])
+        .status();
     activity.finish();
 }
